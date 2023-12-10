01423c4fcdd3d191322b3323a885e2f5610086d3
matpx <9219741+matpx@users.noreply.github.com>
2023-12-10 15:43:34 +0100

    app

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,34 @@
+#define SOKOL_APP_IMPL
+#define SOKOL_LOG_IMPL
+#define SOKOL_GLCORE33
 #include <sokol_app.h>
+#include <sokol_glue.h>
+#include <sokol_log.h>
 
-int main() {
-    
+void init() {}
+
+void frame() {}
+
+void cleanup() {}
+
+void event(const sapp_event *event) {
+  if(event->type == SAPP_EVENTTYPE_KEY_DOWN && event->key_code == SAPP_KEYCODE_ESCAPE) {
+    sapp_request_quit();
+  }  
+}
+
+sapp_desc sokol_main(int argc, char *argv[]) {
+  return {
+      .init_cb = init,
+      .frame_cb = frame,
+      .cleanup_cb = cleanup,
+      .event_cb = event,
+      .width = 1200,
+      .height = 800,
+      .window_title = "Triangle (sokol-app)",
+      .logger =
+          {
+              .func = slog_func,
+          },
+  };
 }
