9dc89d5742cb3f4b6188a1f03bf1e656ec766d04
matpx <9219741+matpx@users.noreply.github.com>
2023-12-21 00:19:24 +0100

    add jolt

--- build.zig
+++ build.zig
@@ -7,6 +7,147 @@ const std = @import("std");
 //     return (try infile.metadata()).accessed() > (try outfile.metadata()).accessed();
 // }
 
+const jolt_src_dir = "thirdparty/JoltPhysics/Jolt";
+
+const jolt_src = &.{
+    jolt_src_dir ++ "/AABBTree/AABBTreeBuilder.cpp",
+    jolt_src_dir ++ "/Core/Color.cpp",
+    jolt_src_dir ++ "/Core/Factory.cpp",
+    jolt_src_dir ++ "/Core/IssueReporting.cpp",
+    jolt_src_dir ++ "/Core/JobSystemSingleThreaded.cpp",
+    jolt_src_dir ++ "/Core/JobSystemThreadPool.cpp",
+    jolt_src_dir ++ "/Core/JobSystemWithBarrier.cpp",
+    jolt_src_dir ++ "/Core/LinearCurve.cpp",
+    jolt_src_dir ++ "/Core/Memory.cpp",
+    jolt_src_dir ++ "/Core/Profiler.cpp",
+    jolt_src_dir ++ "/Core/RTTI.cpp",
+    jolt_src_dir ++ "/Core/Semaphore.cpp",
+    jolt_src_dir ++ "/Core/StringTools.cpp",
+    jolt_src_dir ++ "/Core/TickCounter.cpp",
+    jolt_src_dir ++ "/Geometry/ConvexHullBuilder.cpp",
+    jolt_src_dir ++ "/Geometry/ConvexHullBuilder2D.cpp",
+    jolt_src_dir ++ "/Geometry/Indexify.cpp",
+    jolt_src_dir ++ "/Geometry/OrientedBox.cpp",
+    jolt_src_dir ++ "/Math/Vec3.cpp",
+    jolt_src_dir ++ "/ObjectStream/ObjectStream.cpp",
+    jolt_src_dir ++ "/ObjectStream/ObjectStreamBinaryIn.cpp",
+    jolt_src_dir ++ "/ObjectStream/ObjectStreamBinaryOut.cpp",
+    jolt_src_dir ++ "/ObjectStream/ObjectStreamIn.cpp",
+    jolt_src_dir ++ "/ObjectStream/ObjectStreamOut.cpp",
+    jolt_src_dir ++ "/ObjectStream/ObjectStreamTextIn.cpp",
+    jolt_src_dir ++ "/ObjectStream/ObjectStreamTextOut.cpp",
+    jolt_src_dir ++ "/ObjectStream/SerializableObject.cpp",
+    jolt_src_dir ++ "/ObjectStream/TypeDeclarations.cpp",
+    jolt_src_dir ++ "/Physics/Body/Body.cpp",
+    jolt_src_dir ++ "/Physics/Body/BodyAccess.cpp",
+    jolt_src_dir ++ "/Physics/Body/BodyCreationSettings.cpp",
+    jolt_src_dir ++ "/Physics/Body/BodyInterface.cpp",
+    jolt_src_dir ++ "/Physics/Body/BodyManager.cpp",
+    jolt_src_dir ++ "/Physics/Body/MassProperties.cpp",
+    jolt_src_dir ++ "/Physics/Body/MotionProperties.cpp",
+    jolt_src_dir ++ "/Physics/Character/Character.cpp",
+    jolt_src_dir ++ "/Physics/Character/CharacterBase.cpp",
+    jolt_src_dir ++ "/Physics/Character/CharacterVirtual.cpp",
+    jolt_src_dir ++ "/Physics/Collision/BroadPhase/BroadPhase.cpp",
+    jolt_src_dir ++ "/Physics/Collision/BroadPhase/BroadPhaseBruteForce.cpp",
+    jolt_src_dir ++ "/Physics/Collision/BroadPhase/BroadPhaseQuadTree.cpp",
+    jolt_src_dir ++ "/Physics/Collision/BroadPhase/QuadTree.cpp",
+    jolt_src_dir ++ "/Physics/Collision/CastConvexVsTriangles.cpp",
+    jolt_src_dir ++ "/Physics/Collision/CastSphereVsTriangles.cpp",
+    jolt_src_dir ++ "/Physics/Collision/CollideConvexVsTriangles.cpp",
+    jolt_src_dir ++ "/Physics/Collision/CollideSphereVsTriangles.cpp",
+    jolt_src_dir ++ "/Physics/Collision/CollisionDispatch.cpp",
+    jolt_src_dir ++ "/Physics/Collision/CollisionGroup.cpp",
+    jolt_src_dir ++ "/Physics/Collision/EstimateCollisionResponse.cpp",
+    jolt_src_dir ++ "/Physics/Collision/GroupFilter.cpp",
+    jolt_src_dir ++ "/Physics/Collision/GroupFilterTable.cpp",
+    jolt_src_dir ++ "/Physics/Collision/ManifoldBetweenTwoFaces.cpp",
+    jolt_src_dir ++ "/Physics/Collision/NarrowPhaseQuery.cpp",
+    jolt_src_dir ++ "/Physics/Collision/NarrowPhaseStats.cpp",
+    jolt_src_dir ++ "/Physics/Collision/PhysicsMaterial.cpp",
+    jolt_src_dir ++ "/Physics/Collision/PhysicsMaterialSimple.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/BoxShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/CapsuleShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/CompoundShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/ConvexHullShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/ConvexShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/CylinderShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/DecoratedShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/HeightFieldShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/MeshShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/MutableCompoundShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/OffsetCenterOfMassShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/RotatedTranslatedShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/ScaledShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/Shape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/SphereShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/StaticCompoundShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/TaperedCapsuleShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/Shape/TriangleShape.cpp",
+    jolt_src_dir ++ "/Physics/Collision/TransformedShape.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/ConeConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/Constraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/ConstraintManager.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/ContactConstraintManager.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/DistanceConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/FixedConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/GearConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/HingeConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/MotorSettings.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/PathConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/PathConstraintPath.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/PathConstraintPathHermite.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/PointConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/PulleyConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/RackAndPinionConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/SixDOFConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/SliderConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/SpringSettings.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/SwingTwistConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Constraints/TwoBodyConstraint.cpp",
+    jolt_src_dir ++ "/Physics/DeterminismLog.cpp",
+    jolt_src_dir ++ "/Physics/IslandBuilder.cpp",
+    jolt_src_dir ++ "/Physics/LargeIslandSplitter.cpp",
+    jolt_src_dir ++ "/Physics/PhysicsLock.cpp",
+    jolt_src_dir ++ "/Physics/PhysicsScene.cpp",
+    jolt_src_dir ++ "/Physics/PhysicsSystem.cpp",
+    jolt_src_dir ++ "/Physics/PhysicsUpdateContext.cpp",
+    jolt_src_dir ++ "/Physics/Ragdoll/Ragdoll.cpp",
+    jolt_src_dir ++ "/Physics/SoftBody/SoftBodyCreationSettings.cpp",
+    jolt_src_dir ++ "/Physics/SoftBody/SoftBodyMotionProperties.cpp",
+    jolt_src_dir ++ "/Physics/SoftBody/SoftBodyShape.cpp",
+    jolt_src_dir ++ "/Physics/SoftBody/SoftBodySharedSettings.cpp",
+    jolt_src_dir ++ "/Physics/StateRecorderImpl.cpp",
+    jolt_src_dir ++ "/Physics/Vehicle/MotorcycleController.cpp",
+    jolt_src_dir ++ "/Physics/Vehicle/TrackedVehicleController.cpp",
+    jolt_src_dir ++ "/Physics/Vehicle/VehicleAntiRollBar.cpp",
+    jolt_src_dir ++ "/Physics/Vehicle/VehicleCollisionTester.cpp",
+    jolt_src_dir ++ "/Physics/Vehicle/VehicleConstraint.cpp",
+    jolt_src_dir ++ "/Physics/Vehicle/VehicleController.cpp",
+    jolt_src_dir ++ "/Physics/Vehicle/VehicleDifferential.cpp",
+    jolt_src_dir ++ "/Physics/Vehicle/VehicleEngine.cpp",
+    jolt_src_dir ++ "/Physics/Vehicle/VehicleTrack.cpp",
+    jolt_src_dir ++ "/Physics/Vehicle/VehicleTransmission.cpp",
+    jolt_src_dir ++ "/Physics/Vehicle/Wheel.cpp",
+    jolt_src_dir ++ "/Physics/Vehicle/WheeledVehicleController.cpp",
+    jolt_src_dir ++ "/RegisterTypes.cpp",
+    jolt_src_dir ++ "/Renderer/DebugRenderer.cpp",
+    jolt_src_dir ++ "/Renderer/DebugRendererPlayback.cpp",
+    jolt_src_dir ++ "/Renderer/DebugRendererRecorder.cpp",
+    jolt_src_dir ++ "/Skeleton/SkeletalAnimation.cpp",
+    jolt_src_dir ++ "/Skeleton/Skeleton.cpp",
+    jolt_src_dir ++ "/Skeleton/SkeletonMapper.cpp",
+    jolt_src_dir ++ "/Skeleton/SkeletonPose.cpp",
+    jolt_src_dir ++ "/TriangleGrouper/TriangleGrouperClosestCentroid.cpp",
+    jolt_src_dir ++ "/TriangleGrouper/TriangleGrouperMorton.cpp",
+    jolt_src_dir ++ "/TriangleSplitter/TriangleSplitter.cpp",
+    jolt_src_dir ++ "/TriangleSplitter/TriangleSplitterBinning.cpp",
+    jolt_src_dir ++ "/TriangleSplitter/TriangleSplitterFixedLeafSize.cpp",
+    jolt_src_dir ++ "/TriangleSplitter/TriangleSplitterLongestAxis.cpp",
+    jolt_src_dir ++ "/TriangleSplitter/TriangleSplitterMean.cpp",
+    jolt_src_dir ++ "/TriangleSplitter/TriangleSplitterMorton.cpp",
+};
+
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -21,6 +162,7 @@ pub fn build(b: *std.Build) !void {
         const system_include_paths: []const []const u8 = &.{
             "thirdparty/SDL2/x86_64-w64-mingw32/include/",
             "thirdparty/nvrhi/include",
+            "thirdparty/JoltPhysics/",
             "thirdparty/sokol/",
             "thirdparty/glm/",
             "thirdparty/entt/src/",
@@ -120,6 +262,40 @@ pub fn build(b: *std.Build) !void {
     }
 
     {
+        const jolt = b.addStaticLibrary(.{
+            .name = "jolt",
+            .target = target,
+            .optimize = optimize,
+        });
+
+        const compiler_args: []const []const u8 = &.{
+            // "-DJPH_PROFILE_ENABLED",
+            // "-DJPH_EXTERNAL_PROFILE",
+            // "-DJPH_DEBUG_RENDERER",
+            // "-DJPH_DISABLE_TEMP_ALLOCATOR",
+            // "-DJPH_DISABLE_CUSTOM_ALLOCATOR",
+            // "-DJPH_FLOATING_POINT_EXCEPTIONS_ENABLED",
+            // "-DJPH_CROSS_PLATFORM_DETERMINISTIC",
+            // "-DJPH_DOUBLE_PRECISION",
+            // "-DJPH_USE_SSE4_1",
+            // "-DJPH_USE_SSE4_2",
+            // "-DJPH_USE_F16C",
+            // "-DJPH_USE_LZCNT",
+            // "-DJPH_USE_TZCNT",
+            // "-DJPH_USE_AVX",
+            // "-DJPH_USE_AVX2",
+            // "-DJPH_USE_AVX512",
+            // "-DJPH_USE_FMADD",
+        };
+
+        jolt.addCSourceFiles(jolt_src, compiler_args);
+        jolt.addIncludePath(std.Build.LazyPath{ .path = "thirdparty/JoltPhysics/" });
+        jolt.linkLibC();
+        jolt.linkLibCpp();
+        exe.linkLibrary(jolt);
+    }
+
+    {
         const system_libs: []const []const u8 = if (exe.target.isWindows())
             &.{
                 "gdi32",
@@ -141,7 +317,9 @@ pub fn build(b: *std.Build) !void {
 
     {
         exe.addObjectFile(std.Build.LazyPath{ .path = "thirdparty/SDL2/x86_64-w64-mingw32/lib/libSDL2.a" });
+        // exe.addObjectFile(std.Build.LazyPath{ .path = "thirdparty/SDL2/x86_64-w64-mingw32/lib/libSDL2.dll.a" });
         exe.addObjectFile(std.Build.LazyPath{ .path = "thirdparty/SDL2/x86_64-w64-mingw32/lib/libSDL2main.a" });
+        // b.installBinFile("thirdparty/SDL2/x86_64-w64-mingw32/bin/SDL2.dll", "SDL2.dll");
     }
 
     // {
